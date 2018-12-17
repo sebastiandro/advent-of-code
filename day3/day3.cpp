3b75d501ca5b3c9e14b242cdeae7ed91dd73035f
Sebastian Nilsson <sebbehebbe@gmail.com>
2018-12-17 15:16:50 +0100

    Day 3 part 1

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 int main()
 {
-    int A[1000][1000];
+    // Creating the matrix
+    int N = 1000;
+    int A[1000][1000] = {0};
+
     string line;
     char id_delim = '@';
     char pos_delim = ':';
     char xy_delim = ',';
     char size_delim = 'x';
+    int left_margin = 0;
+    int top_margin = 0;
     int x;
     int y;
     int width;
@@ -23,6 +28,10 @@ int main()
     int size_end_idx;
     int size_str_len;
 
+    int row;
+    int col;
+    int k = 0;
+
     string pos_str;
     string size_str;
 
@@ -31,20 +40,56 @@ int main()
     {
         while (getline(inputfile, line))
         {
+            k = k + 1;
+
             // Extract position
             pos_start_idx = line.find(id_delim);
             pos_end_idx = line.find(pos_delim);
             pos_str_len = pos_end_idx - pos_start_idx;
             pos_str = line.substr(pos_start_idx + 1, pos_str_len);
 
+            // Margin same as x and y since we start from idx 0
             x = stoi(pos_str.substr(0, pos_str.find(xy_delim)));
             y = stoi(pos_str.substr(pos_str.find(xy_delim) + 1, pos_str.find(pos_delim)));
 
             // Extract size
             size_start_idx = line.find(pos_delim);
-            size_end_idx = line.length - 1;
+            size_end_idx = line.length() - 1;
+            size_str = line.substr(size_start_idx + 1, size_end_idx);
 
-            exit(1);
+            width = stoi(size_str.substr(0, size_str.find(size_delim)));
+            height = stoi(size_str.substr(size_str.find(size_delim) + 1, size_str.length() - 1));
+
+            // We fill the blank spots
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    int row = y + j;
+                    int col = x + i;
+                    A[row][col] += 1;
+                }
+            }
         }
     }
+
+    int overlaps = 0;
+
+    for (int m = 0; m < N; m++)
+    {
+        for (int n = 0; n < N; n++)
+        {
+
+            if (A[m][n] > 1)
+            {
+                overlaps = overlaps + 1;
+            }
+        }
+    }
+
+    printf("Number of overlaps: %i \n", overlaps);
+
+    // Number 2
+
+    return 0;
 }
