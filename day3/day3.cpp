ce5e814e8f5cca6c2e2e8d66fe2c44c87b0516fd
Sebastian Nilsson <sebbehebbe@gmail.com>
2018-12-22 18:50:51 +0100

    Day3 complete

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+const int N = 1000;
 
-int main()
+struct claim
 {
-    // Creating the matrix
-    int N = 1000;
-    int A[1000][1000] = {0};
+    int id, x, y, width, height;
+};
 
-    string line;
-    char id_delim = '@';
-    char pos_delim = ':';
-    char xy_delim = ',';
-    char size_delim = 'x';
-    int left_margin = 0;
-    int top_margin = 0;
+claim decode_line(string line)
+{
+
     int x;
     int y;
     int width;
     int height;
+
+    int id;
 
     int pos_start_idx;
     int pos_end_idx;
@@ -28,68 +26,115 @@ int main()
     int size_end_idx;
     int size_str_len;
 
-    int row;
-    int col;
-    int k = 0;
-
     string pos_str;
     string size_str;
+
+    char id_delim = '@';
+    char pos_delim = ':';
+    char xy_delim = ',';
+    char size_delim = 'x';
+
+    // Extract ID
+    pos_start_idx = line.find(id_delim);
+    id = stoi(line.substr(1, pos_start_idx));
+
+    // Extract position
+    pos_start_idx = line.find(id_delim);
+    pos_end_idx = line.find(pos_delim);
+    pos_str_len = pos_end_idx - pos_start_idx;
+    pos_str = line.substr(pos_start_idx + 1, pos_str_len);
+
+    // Margin same as x and y since we start from idx 0
+    x = stoi(pos_str.substr(0, pos_str.find(xy_delim)));
+    y = stoi(pos_str.substr(pos_str.find(xy_delim) + 1, pos_str.find(pos_delim)));
+
+    // Extract size
+    size_start_idx = line.find(pos_delim);
+    size_end_idx = line.length() - 1;
+    size_str = line.substr(size_start_idx + 1, size_end_idx);
+
+    width = stoi(size_str.substr(0, size_str.find(size_delim)));
+    height = stoi(size_str.substr(size_str.find(size_delim) + 1, size_str.length() - 1));
+
+    claim decoded_line = {id, x, y, width, height};
+    return decoded_line;
+}
+
+int main()
+{
+    // Creating the matrix
+    static int A[N * N][2] = {0};
+
+    string line;
+    claim clm;
 
     ifstream inputfile("input.txt");
     if (inputfile.is_open())
     {
         while (getline(inputfile, line))
         {
-            k = k + 1;
 
-            // Extract position
-            pos_start_idx = line.find(id_delim);
-            pos_end_idx = line.find(pos_delim);
-            pos_str_len = pos_end_idx - pos_start_idx;
-            pos_str = line.substr(pos_start_idx + 1, pos_str_len);
+            clm = decode_line(line);
 
-            // Margin same as x and y since we start from idx 0
-            x = stoi(pos_str.substr(0, pos_str.find(xy_delim)));
-            y = stoi(pos_str.substr(pos_str.find(xy_delim) + 1, pos_str.find(pos_delim)));
-
-            // Extract size
-            size_start_idx = line.find(pos_delim);
-            size_end_idx = line.length() - 1;
-            size_str = line.substr(size_start_idx + 1, size_end_idx);
-
-            width = stoi(size_str.substr(0, size_str.find(size_delim)));
-            height = stoi(size_str.substr(size_str.find(size_delim) + 1, size_str.length() - 1));
-
-            // We fill the blank spots
-            for (int i = 0; i < width; i++)
+            for (int i = clm.y; i < clm.y + clm.height; i++)
             {
-                for (int j = 0; j < height; j++)
+                for (int j = clm.x; j < clm.x + clm.width; j++)
                 {
-                    int row = y + j;
-                    int col = x + i;
-                    A[row][col] += 1;
+                    int p = i * N + j;
+                    A[p][0] = A[p][0] + 1;
+                    A[p][1] = clm.id;
                 }
             }
         }
     }
 
+    inputfile.close();
+
+    // Question 1
     int overlaps = 0;
+    int last_id = -1;
+    int curr_id_overlaps = -1;
+    int no_overlap_claim_id;
 
-    for (int m = 0; m < N; m++)
+    for (int i = 0; i < N * N; i++)
     {
-        for (int n = 0; n < N; n++)
         {
-
-            if (A[m][n] > 1)
+            if (A[i][0] > 1)
             {
-                overlaps = overlaps + 1;
+                overlaps++;
+            }
+        }
+    }
+
+    // Question 2
+    no_overlap_claim_id = -1;
+    ifstream inputfile2("input.txt");
+    if (inputfile2.is_open())
+    {
+        while (getline(inputfile2, line))
+        {
+            int current_claims_overlaps = 0;
+            claim clm = decode_line(line);
+            for (int i = clm.y; i < clm.y + clm.height; i++)
+            {
+                for (int j = clm.x; j < clm.x + clm.width; j++)
+                {
+                    int p = i * N + j;
+                    if (*A[p] > 1)
+                    {
+                        current_claims_overlaps++;
+                    }
+                }
+            }
+            if (current_claims_overlaps == 0)
+            {
+                no_overlap_claim_id = clm.id;
+                break;
             }
         }
     }
 
     printf("Number of overlaps: %i \n", overlaps);
-
-    // Number 2
-
+    printf("No overlap id: %i \n", no_overlap_claim_id);
     return 0;
 }
