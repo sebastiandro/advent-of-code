d8ab918e8dbace1ca16d1f4eafc334c5c50e8ec6
Sebastian Nilsson <sebbehebbe@gmail.com>
2018-12-27 19:41:00 +0100

    Day 4 part 2

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -26,6 +26,7 @@ void add_guards(vector<string> &inpts, map<int, tuple<int, vector<int>>> &guard_
 void update_guard_sleep(tuple<int, vector<int>> &tp, int fell_asleep_min, int woke_up_min);
 int find_worst_guard(map<int, tuple<int, vector<int>>> &guard_minutes);
 int find_worst_minute(int guard_id, map<int, tuple<int, vector<int>>> &guard_minutes);
+tuple<int, int> most_frequent_asleep(map<int, tuple<int, vector<int>>> &guard_minutes);
 
 static int runs = 0;
 
@@ -51,12 +52,55 @@ int main()
     cout << "Worst Guard ID: " << worst_guard_id << endl;
     cout << "Worst Minute for that Guard: " << worst_minute << endl;
     cout << "Number of iterations: " << runs << endl;
-    // Populate
 
+    // Populate
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(t2 - t1).count();
     cout << "Execution time: " << duration << " ms" << endl;
+
+    // Part 2
+    tuple<int, int> most_freq_tp = most_frequent_asleep(guard_minutes);
+    int p2_minute = get<0>(most_freq_tp);
+    int p2_guard_id = get<1>(most_freq_tp);
+    cout << "Guard " << p2_guard_id << " sleeps the most at " << p2_minute << endl;
     return 0;
+}
+
+tuple<int, int> most_frequent_asleep(map<int, tuple<int, vector<int>>> &guard_minutes)
+{
+
+    map<int, tuple<int, vector<int>>>::iterator it;
+    it = guard_minutes.begin();
+    int total_max_minutes_slept = 0;
+    int all_most_frequent_minute = 0;
+    int guard_id = 0;
+    while (it != guard_minutes.end())
+    {
+        tuple<int, vector<int>> tp = it->second;
+        vector<int> minutes = get<1>(tp);
+        int max_slept_minutes = 0;
+        int most_frequent_minute = 0;
+        for (int i = 0; i < minutes.size(); i++)
+        {
+            int minutes_slept = minutes[i];
+            if (minutes_slept > max_slept_minutes)
+            {
+                most_frequent_minute = i;
+                max_slept_minutes = minutes_slept;
+            }
+        }
+
+        if (max_slept_minutes > total_max_minutes_slept)
+        {
+            total_max_minutes_slept = max_slept_minutes;
+            guard_id = it->first;
+            all_most_frequent_minute = most_frequent_minute;
+        }
+
+        it++;
+    }
+
+    return make_tuple(all_most_frequent_minute, guard_id);
 }
 
 void add_guards(vector<string> &inpts, map<int, tuple<int, vector<int>>> &guard_minutes)
